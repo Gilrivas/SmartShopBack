5d5c1604f5a51407c878de8fb8ead053020fd8d2
Jorge Gil Rivas <jorged1509@gmail.com>
2024-02-13 15:55:17 +0100

    installed jwt to create users token

--- login.js
+++ login.js
@@ -1,19 +1,30 @@
 // login.js
 const { User } = require('./database');
+const jwt = require('jsonwebtoken');
 
 async function login(req, res) {
         const { user_name, user_password } = req.body;
-      
+       
         const data = await User.findOne({ where: { user_name, user_password } });
       
       
         if (data) {
-          console.log('nice')
+          const token = generateAuthToken(data);
+          res.json({ token });
+          console.log('nice', token)
       
         } else {
           res.status(401).send('Invalid email or password');
         }
-      
+
+        function generateAuthToken(userId) {
+          const payload = { userId };
+          const secret = 'true';
+          const token = jwt.sign(payload, secret);
+          return token;
+        }
+        
+        
 }
 
 module.exports = login;
